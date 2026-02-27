569c1a09c7d67bb88e54b5e88f1e0fcc244e91b5
emad-aisha <emadaisha2024@gmail.com>
2026-02-27 08:22:11 +0300

    init

---
+++ Assets/Scripts/Interaction/Interact.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    // function pointer that
+    //  returns void
+    //  takes in no parameters
+    delegate void getDrink();
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+
+}
