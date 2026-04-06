18e3287f6a4e745218713c465dc96a432999a7be
emad-aisha <emadaisha2024@gmail.com>
2026-04-06 08:18:07 +0300

    Interaction Revamp

--- Assets/Scripts/Interfaces/IMoney.cs
+++ Assets/Scripts/Interfaces/IMoney.cs
@@ -1,14 +1,19 @@
 
 
 public interface IMoney {
-    public void ResetInfo(int currMoney) {
+    public void ClearInfo(int currMoney) {
         SetDrinkInfo("nothing", "0");
         MenuManager.instance.SetPlayerMoney(currMoney.ToString());
+    }
+
+    public void ResetDrinkInfo(ref Drink[] drinks) {
+        for (int i = 0; i < drinks.Length; i++) {
+            drinks[i].ResetInfo();
+        }
     }
 
     void SetDrinkInfo(string finalDrink, string cost) {
         MenuManager.instance.SetFinalDrink(finalDrink);
         MenuManager.instance.SetCost(cost);
     }
-
 }
--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
-    // TODO: change into an array of drinks
-    // then make drinks use an array to differentiate
     [SerializeField] Drink[] drinks;
 
     [Header("Debug")]
@@ -12,30 +10,45 @@ public class Inventory : MonoBehaviour {
     public int currMoney = 0;
     public int currPrice;
 
-
-
     public void Interact(Collider interactable) {
-        FizzyDrink inputFizzyDrink = interactable.GetComponent<FizzyDrink>();
-        Coffee inputCoffee = interactable.GetComponent<Coffee>();
         IMoney money = interactable.GetComponent<IMoney>();
 
-        // TODO: there is probably a way to simplify this
-        if (inputFizzyDrink != null && IsOtherDrinksOff(DrinkType.FizzyDrink)) {
-            inputFizzyDrink.Interact(ref drinks[(int)DrinkType.FizzyDrink], ref currPriority);
-            SetDrinkInfo(drinks[(int)DrinkType.FizzyDrink]);
-        }
-        else if (inputCoffee != null && IsOtherDrinksOff(DrinkType.Coffee)) {
-            inputCoffee.Interact(ref drinks[(int)DrinkType.Coffee], ref currPriority);
-            SetDrinkInfo(drinks[(int)DrinkType.Coffee]);
-        }
+        Drink drinkMachine = interactable.GetComponent<Drink>();
+        DrinkType drinkType = GetDrinkType(interactable);
+
+        if (drinkMachine != null) { Interact(drinkMachine, drinkType); }
         else if (money != null) {
             AddMoney();
-            ResetDrinkInfo();
-            money.ResetInfo(currMoney);
+            money.ResetDrinkInfo(ref drinks);
+            money.ClearInfo(currMoney);
         }
-
     }
 
+    // setters
+    void SetDrinkInfo(Drink drink) {
+        currPrice = drink.price;
+        MenuManager.instance.SetFinalDrink(drink.finalDrinkName);
+        MenuManager.instance.SetCost(drink.price.ToString());
+    }
+
+    // getters
+    DrinkType GetDrinkType(Collider interactable) {
+        FizzyDrink fizzyComponent = interactable.GetComponent<FizzyDrink>();
+        Coffee coffeeComponent = interactable.GetComponent<Coffee>();
+
+        if (fizzyComponent != null) return DrinkType.FizzyDrink;
+        else if (coffeeComponent != null) return DrinkType.Coffee;
+
+        return DrinkType.Null;
+    }
+
+
+    void Interact(Drink drinkMachine, DrinkType drink) {
+        if (!IsOtherDrinksOff(drink)) return;
+
+        drinkMachine.Interact(ref drinks[(int)drink], ref currPriority);
+        SetDrinkInfo(drinks[(int)drink]);
+    }
     bool IsOtherDrinksOff(DrinkType drinkToCompare) {
         for (int i = 0; i < drinks.Length; i++) {
             if (i == (int)drinkToCompare) continue;
@@ -44,17 +57,7 @@ public class Inventory : MonoBehaviour {
         return true;
     }
 
-    void SetDrinkInfo(Drink drink) {
-        currPrice = drink.price;
-        MenuManager.instance.SetFinalDrink(drink.finalDrinkName);
-        MenuManager.instance.SetCost(drink.price.ToString());
-    }
-
-    void ResetDrinkInfo() {
-        for (int i = 0; i < drinks.Length; i++) {
-            drinks[i].ResetInfo();
-        }
-    }
+    // money help
     void AddMoney() {
         currMoney += currPrice;
         currPrice = 0;
